9fa2d074478eff1a2f201a699dfb71e6542b0de8
korrescatherine <cathy.korres@gmail.com>
2019-06-12 00:43:14 -0300

    path to access images after upload
    + cute pic of a cat

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://<user>:<password>@cluster0-aygqp.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true   
 });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
